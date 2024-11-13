e9e3147594daddfcc82cbacab3a14a0cc30ceaea
Ike Griss Salas <54322188+MalachiteWind@users.noreply.github.com>
2024-11-12 22:05:47 -0800

    feat: create tensordata for lstm training through mitosis.
    Refactor tensor_data to seperate steps for creating TensorDataset.
    Return results dictionary containg train, val, test TensorDataset to be
    passed through LSTM training module. Also return transformer and
    dataset geenrated by generate_toy_dataset().

---
+++ exp/config.py
@@ -0,0 +1,5 @@
+tensor_data_lookup = {
+    "seed": {"default": 1234},
+    "lags": {"default": 30},
+    "train_len": {"default": 0.8}
+}
--- exp/tensor_data.py
+++ exp/tensor_data.py
@@ -1,84 +1,106 @@
-from pydmdeep.data import generate_toy_dataset, create_tensor_data
+from pydmdeep.data import generate_toy_dataset
 import warnings
 from sklearn.preprocessing import MinMaxScaler
 from torch.utils.data import TensorDataset
 import numpy as np
+from numpy.random import Generator
 import torch
-from pydmdeep.types import Float2D, Int1D
+from pydmdeep.types import Float2D, Int1D, Float3D
+from typing import List
 
 DEVICE = "cuda" if torch.cuda.is_available() else "CPU"
 
 
 
-def run(seed:int):
-    # rng = np.random.default_rng(seed=seed)
+def run(seed:int, lags:int,train_len:float):
+    rng = np.random.default_rng(seed=seed)
+    if DEVICE == "CPU":
+        warnings.warn("Using CPU instead of cuda.", stacklevel=2)
+
     dataset = generate_toy_dataset(tmax=8*np.pi,nt =129*8,nx= 65*8)
     U,S,Vh = np.linalg.svd(dataset["time_delay1"])
 
+    nt, nx = Vh.shape
+
+    train_idx, val_idx = _train_val_idxs(nt-lags,train_len=train_len, rng=rng)
+    test_idx = val_idx[1::2]
+    val_idx = val_idx[::2]
+
+    min_max_scalaer = MinMaxScaler()
+    min_max_scalaer.fit(Vh[train_idx])
+
+    Vh_transformed = min_max_scalaer.transform(Vh)
+
+    data_seq_in, data_seq_out = _create_data_seq(Vh_transformed,lags=lags)
+    
     train, val, test = create_tensor_data(
-        data_mat=Vh,
-        lags=32,
-        train_len=0.9,
+        data_seq_in,
+        data_seq_out,
+        idxs=[train_idx,val_idx,test_idx],
         device=DEVICE
-
     )
+
+    results = {
+        "main": (train,val,test),
+        "transformer": min_max_scalaer,
+        "dataset": dataset,
+    }
     
-    return train,val,test
+    return results
 
-
-def _train_val_idxs(n_len: int, train_len: float,) -> tuple[Int1D, Int1D]:
-    idxs = np.arange(n_len)
-    np.random.shuffle(idxs)
-    n_train = int(n_len * train_len)
-    return idxs[:n_train], idxs[n_train:]
-
-def create_tensor_data(
-    data_mat: Float2D, lags: int, train_len: float, device: str = "cpu"
-) -> tuple[TensorDataset, TensorDataset, TensorDataset]:
+def create_tensor_data(in_data:Float2D, out_data:Float2D, idxs: List[Int1D], device:str):
     """
-    
+    Convert data_sequences and target labels to TensorDataset based on desired indicies.
+    Send data to gpu. 
 
     Parameters:
     ----------
-    data_mat: shape (nt,nx) samples are the rows.
+    in_data: input data sequence
+    out_data: target_labels
+    idxs: List of indicies to split into seperate Tensordatasets. i.e. train/val/test.
+    device: where to store data on. `cuda` or `cpu`.
+
+    Returns:
+    -------
+    tensor_dataset: list of TensorDatasets to be passed into Dataloaders or torch module.
+
     """
-    nt, nx = data_mat.shape
-    train_idxs, val_idxs = _train_val_idxs(nt - lags, train_len=train_len)
-    test_idxs = val_idxs[1::2]
-    val_idxs = val_idxs[::2]
+    tensor_datasets = []
+    for idx in idxs:
+        in_data_tensor = torch.tensor(in_data[idx],dtype =torch.float32).to(device)
+        out_data_tensor = torch.tensor(out_data[idx],dtype =torch.float32).to(device)
+        tensor_datasets.append(TensorDataset(in_data_tensor,out_data_tensor))
+    return tensor_datasets
 
-    min_max_scaler = MinMaxScaler()
-    min_max_scaler.fit(data_mat[train_idxs])
-
-    transformed_data = min_max_scaler.transform(data_mat)
-
-    nt, nx = data_mat.shape
-
-    data_seq = np.zeros((nt - lags, lags, nx))
-    for idx in range(nt - lags):
-        data_seq[idx] = transformed_data[idx : idx + lags]
-
-    train_data_in = torch.tensor(data=data_seq[train_idxs], dtype=torch.float32).to(device)
-    val_data_in = torch.tensor(data=data_seq[val_idxs], dtype=torch.float32).to(device)
-    test_data_in = torch.tensor(data=data_seq[test_idxs], dtype=torch.float32).to(device)
-
+def _create_data_seq(data:Float2D, lags:int) -> tuple[Float3D, Float2D]:
+    """
+    Convert dataset into data sequences (both input and output targets) for LSTM 
+    learning.
     
-    if device == "cpu":
-        warnings.warn("CPU used instead of GPU.", stacklevel=2)
+    Parameters:
+    ----------
+    data: matrix containing timeseries (nt,nx)
+    lags: size of input mapping to be created for data_seq_in
 
-    train_data_out = torch.tensor(
-        transformed_data[train_idxs + lags - 1], dtype=torch.float32
-    ).to(device)
-    val_data_out = torch.tensor(
-        transformed_data[val_idxs + lags - 1], dtype=torch.float32
-    ).to(device)
-    test_data_out = torch.tensor(
-        transformed_data[test_idxs + lags - 1], dtype=torch.float32
-    ).to(device)
+    Returns:
+    -------
+    data_seq_in: sequence inputs
+    data_seq_out: target labels for sequences. 
+    """
+    nt, nx = data.shape
+    data_seq_in = np.zeros((nt-lags,lags,nx))
+    data_seq_out = np.zeros(((nt-lags,nx)))
+    for idx in range(nt-lags):
+        data_seq_in[idx] = data[idx:idx+lags]
+        data_seq_out[idx] = data[idx+lags]
+    return data_seq_in, data_seq_out
 
-    train_data = TensorDataset(train_data_in, train_data_out)
-    val_data = TensorDataset(val_data_in, val_data_out)
-    test_data = TensorDataset(test_data_in, test_data_out)
-
-    return train_data, val_data, test_data, min_max_scaler
-
+def _train_val_idxs(n_len: int, train_len: float,rng:Generator) -> tuple[Int1D, Int1D]:
+    """
+    Create train and val/test indices for n_len indicies where train_len inidicates
+    number of indicies to be used for training.
+    """
+    idxs = np.arange(n_len)
+    rng.shuffle(idxs)
+    n_train = int(n_len * train_len)
+    return idxs[:n_train], idxs[n_train:]
