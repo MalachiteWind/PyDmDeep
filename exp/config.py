ec3f1715d821d9bf421ecf751e7ae5cd3638f58e
Ike Griss Salas <54322188+MalachiteWind@users.noreply.github.com>
2024-11-17 00:24:52 -0800

    cln: add comment

--- exp/config.py
+++ exp/config.py
@@ -7,7 +7,7 @@ tensor_data_lookup = {}
 lstm_train_lookup = {
     "num_layers": {"small": 10},
     "num_epochs": {"1k": 1000},
-    "opt": {"SGD": (optim.SGD, {"lr": 0.01})},
+    "opt": {"SGD": (optim.SGD, {"lr": 0.01})}, # (opt, opt keywords)
     "loss": {"MSE": nn.MSELoss},
     "dataloader_kws": {"small_batch": {"batch_size": 50, "shuffle": True}},
 }
