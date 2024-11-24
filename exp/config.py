0be443b1b95399499bddab90a86638b0158d4008
Ike Griss Salas <54322188+MalachiteWind@users.noreply.github.com>
2024-11-24 12:49:36 -0800

    update config

--- exp/config.py
+++ exp/config.py
@@ -6,8 +6,11 @@ tensor_data_lookup = {}
 
 lstm_train_lookup = {
     "num_layers": {"small": 10},
-    "num_epochs": {"1k": 1000},
-    "opt": {"SGD": (optim.SGD, {"lr": 0.01})}, # (opt, opt keywords)
+    "num_epochs": {"1k": 1000, "2k": 2000},
+    "opt": {
+        "SGD": (optim.SGD, {"lr": 0.01}),
+        "SGD_05": (optim.SGD, {"lr": 0.05}),
+        }, # (opt, opt keywords)
     "loss": {"MSE": nn.MSELoss},
     "dataloader_kws": {"small_batch": {"batch_size": 50, "shuffle": True}},
     "model_trainer_kws": {
